325b3b5e9a15a3ae89a85972345149feb4450f8a
Abdelrahman Ahmed <abdoahmed132001@gmail.com>
2023-05-15 00:01:46 -0700

    Restaurant website

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(".arrowup").click( function(){
+   $("html,body").animate({scrollTop:"0"},1000)
+})
+
+let scrolnav= $("#about").offset().top
+$(".colonav").css({color:"white"})
+$(".arrowup").fadeOut(100) 
+$(window).scroll(function(){
+
+          
+          let ascroltio = $(window).scrollTop()
+
+    
+         if(ascroltio>=scrolnav)
+         {
+            $(".arrowup").fadeIn(100)
+           
+         }
+         else
+         {
+         
+            $(".arrowup").fadeOut(100)
+         }
+         
+         
+})
+
+
+$(".nav-link").click(function () {
+
+   let selectsection = $(this).attr("href")
+
+   let mytop = $(selectsection).offset().top
+   $("html,body").animate({ scrollTop: mytop }, 1000)
+
+})
